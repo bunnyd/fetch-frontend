4dd7f46bc485325a35eec63690372a270153fc72
Nancy Do <nancydo7@gmail.com>
2018-11-04 16:29:46 -0600

    updated signup

--- src/views/Components/pages/SignupUser.js
+++ src/views/Components/pages/SignupUser.js
@@ -48,6 +48,7 @@ class SignupUser extends React.Component {
 
     const data = new FormData(e.target);
     console.log(data);
+    debugger;
 
     fetch(`https://nameless-everglades-31188.herokuapp.com/owners/`, {
       method: "POST",
